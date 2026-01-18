a7c6a1af9391f5cfc660dc71bb5f8a109d129847
osaama06 <0rumoosh@gmail.com>
2026-01-19 02:33:40 +0300

    ghg

--- app/(pages)/orders/page.js
+++ app/(pages)/orders/page.js
@@ -1,8 +1,6 @@
 // app/(pages)/orders/page.js
 import { Suspense } from 'react';
 import OrdersPageContent from './OrdersPageContent';
-import { getCurrentUser } from '@/lib/server-auth';
-import { redirect } from 'next/navigation';
 
 export default function OrdersPage() {
   return (
