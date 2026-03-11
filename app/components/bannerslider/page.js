17746d5f09ffd9f8d3c854beca73719a64752cdf
osaama06 <0rumoosh@gmail.com>
2026-03-11 05:56:39 +0300

    fix: update BannerSlider height to 91vh for better responsiveness

--- app/components/bannerslider/page.js
+++ app/components/bannerslider/page.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect, useCallback, useRef } from "react";
 import Image from "next/image";
 import "@/styles/components/BannerSlider.css";
 
+
 const ChevronLeft = () => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" width="24" height="24">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
